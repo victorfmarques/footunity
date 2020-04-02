74782b05a66ff7710914fbf1f085530ee48c5c68
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-04-02 20:32:10 -0300

    Teste de multiplas medidas

--- LocalApplication/Sensor.py
+++ LocalApplication/Sensor.py
@@ -15,20 +15,22 @@ GPIO.output(TRIG, False)
 print("Waiting For Sensor To Settle")
 time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+for i in range(1, 10):
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO) == 0:
-    pulse_start = time.time()
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
 
-while GPIO.input(ECHO) == 1:
-    pulse_end = time.time()
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start
+    pulse_duration = pulse_end - pulse_start
 
-distance = pulse_duration * 17150
-distance = round(distance, 2)
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
 
-print("Distance:" + str(distance) + " cm")
+    print("Distance:" + str(distance) + " cm")
+
 GPIO.cleanup()
