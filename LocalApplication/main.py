9bdec13e6fcc98a7ddae8c8cb226cdd660ac7f78
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-04-13 16:17:38 -0300

    teste nova lib

---
+++ LocalApplication/main.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+import sensor_hy_srf05
+
+GPIO.setmode(GPIO.BCM)
+
+sensor = sensor_hy_srf05.SRF05(trigger_pin = 23, echo_pin = 24)
+while True:
+    print(sensor.measure())
