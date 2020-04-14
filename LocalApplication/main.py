ca617e15739f4291ed461fa6d0e56a032cf059ee
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-04-13 21:29:55 -0300

    i

--- LocalApplication/main.py
+++ LocalApplication/main.py
@@ -3,6 +3,6 @@ import sensor_hy_srf05
 
 GPIO.setmode(GPIO.BCM)
 
-sensor = sensor_hy_srf05.SRF05(trigger_pin = 22, echo_pin = 24)
+sensor = sensor_hy_srf05.SRF05(trigger_pin = 23, echo_pin = 24)
 while True:
     print(sensor.measure())
