96f8583243381f4bff8d6c94fa01ac94a3db57d9
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-04-25 13:21:14 -0300

    Fixing of sensor classes, and linking between functionalities has been added

--- LocalApplication/sensor_hc_sr04.py
+++ LocalApplication/sensor_hc_sr04.py
@@ -4,23 +4,20 @@ import helper as lib_helper
 
 
 class hc_sr04(object):
-    __GPIO =  GPIO
     __trigger_pin = 0
-    __echo_pin= 0
-	
+    __echo_pin = 0
+
     def __init__(self, trigger_pin, echo_pin):
         # Choosing BCM setmode
-        GPIO.setmode(GPIO.BCM)
+        # GPIO.setmode(GPIO.BCM)
         self.__trigger_pin = trigger_pin
         self.__echo_pin = echo_pin
 
-        print("OOOOOOOOOOOOOO -> "+ str(self.__GPIO))
         print(str(self.__trigger_pin))
 
         # Setting trigger and echo pins
-        GPIO.setup(self.__trigger_pin, GPIO.OUT)
-        GPIO.setup(self.__echo_pin, GPIO.IN)
-        self.GPIO = GPIO
+        # GPIO.setup(self.__trigger_pin, GPIO.OUT)
+        # GPIO.setup(self.__echo_pin, GPIO.IN)
 
     def get_distance_cm(self):
         # Setting local variables
@@ -29,32 +26,36 @@ class hc_sr04(object):
         sig_time = 0
         distance = 0
 
-        print("GPIO -> " + str(self.__GPIO))
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.__trigger_pin, GPIO.OUT)
+        GPIO.setup(self.__echo_pin, GPIO.IN)
+
+        print("GPIO -> " + str(GPIO))
         print("get_distance " + str(self.__trigger_pin))
 
         try:
             # Forcing trigger pin to low
-            self.__GPIO.output(self.__trigger_pin, False)
+            GPIO.output(self.__trigger_pin, False)
             time.sleep(2)
 
             # Triggering a pulse
-            self.__GPIO.output(self.__trigger_pin, True)
+            GPIO.output(self.__trigger_pin, True)
             time.sleep(0.00001)
-            self.__GPIO.output(self.__trigger_pin, False)
+            GPIO.output(self.__trigger_pin, False)
 
             # Setting start time until the pulse is not captured back
-            while not self.__GPIO.input(self.__echo_pin):
+            while not GPIO.input(self.__echo_pin):
                 start = time.time()
 
             # Setting end time until the pulse is listened
-            while self.__GPIO.input(self.__echo_pin):
+            while GPIO.input(self.__echo_pin):
                 end = time.time()
 
             # Calculating the pulse time
             sig_time = end - start
             dht11 = lib_dht11.dht11(4)
 
-            distance = sig_time * (int(lib_helper.get_speed_of_sound())/2)
+            distance = sig_time * (int(lib_helper.get_speed_of_sound()) / 2)
 
         except Exception as e:
             # Handling exception
@@ -62,7 +63,6 @@ class hc_sr04(object):
 
         finally:
             # Clean GPIO pins
-            self.__GPIO.cleanup()
+            GPIO.cleanup()
 
         return distance
-
