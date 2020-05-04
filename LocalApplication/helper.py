9e48e4cfa708ef258559a351b7cf48846bcf22a1
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-05-03 22:27:13 -0300

    Updating helper file

--- LocalApplication/helper.py
+++ LocalApplication/helper.py
@@ -1,4 +1,4 @@
-import LocalApplication.sensor_dht11 as lib_dht11
+import sensor_dht11 as lib_dht11
 
 
 def getMAC(interface='eth0'):
@@ -10,7 +10,7 @@ def getMAC(interface='eth0'):
     return str_mac_address[0:17]
 
 
-def get_speed_of_sound(self):
+def get_speed_of_sound():
     dht11 = lib_dht11.dht11(4)
-    return 331 + 0.59 * dht11.get_temperature
+    return 331 + 0.59 * dht11.get_temperature()
 
