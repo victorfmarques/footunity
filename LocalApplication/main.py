5545d237880cc0317d66a928780ec6b0e5ccb930
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-05-03 22:27:33 -0300

    Updating main file

--- LocalApplication/main.py
+++ LocalApplication/main.py
@@ -1,7 +1,4 @@
-import RPi.GPIO as GPIO
-import LocalApplication.sensor_hc_sr04 as lib_hc_sr04
-
-GPIO.setmode(GPIO.BCM)
+import sensor_hc_sr04 as lib_hc_sr04
 
 sensor = lib_hc_sr04.hc_sr04(trigger_pin=23, echo_pin=24)
 while True:
