45a021120cd200aa3f0530d7153dec55daf0d3a1
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-05-09 12:47:59 -0300

    modified:   api.py

--- LocalApplication/api.py
+++ LocalApplication/api.py
@@ -5,6 +5,7 @@ import sensor_hc_sr04 as lib_hc_sr04
 
 def callback(ch, method, properties, body):
     print(body.decode('utf8'))
+    print(str(body[0]))
     sensor = lib_hc_sr04.hc_sr04(trigger_pin=23, echo_pin=24)
     print(str(sensor.get_distance_cm()))
     ch.basic_ack(delivery_tag=method.delivery_tag)
