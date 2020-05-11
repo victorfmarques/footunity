373ab7860657ef05ea43bd0390f0d8a585b0f6f0
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-05-11 20:44:59 -0300

    test 1 api communicator

--- LocalApplication/api.py
+++ LocalApplication/api.py
@@ -2,18 +2,6 @@ KKKKKKKKKKKKimport pika
 import json
 import sensor_hc_sr04 as lib_hc_sr04
 
-USER_ID = 0
-
-def callback(ch, method, properties, body):
-    obj = json.loads(body)
-    USER_ID = int(obj['UserId'])
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-def measure_procedure(user_id):
-    sensor = lib_hc_sr04.hc_sr04(trigger_pin=23, echo_pin=24)
-    size = 45 - sensor.get_distance_cm
-
-
 
 
 class ApiCommunicator(object):
@@ -23,6 +11,18 @@ class ApiCommunicator(object):
         self.parameters = pika.ConnectionParameters('201.75.200.31', 5672, '/', self.credentials)
         self.connection = pika.BlockingConnection(self.parameters)
         self.channel = self.connection.channel()
+        self.user_id = 0
+        self.size = 0
+
+    def measure_procedure(self):
+        sensor = lib_hc_sr04.hc_sr04(trigger_pin=23, echo_pin=24)
+        self.size = 45 - sensor.get_distance_cm
+
+    def callback(self, ch, method, properties, body):
+        obj = json.loads(body)
+        self.user_id = int(obj['UserId'])
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        self.measure_procedure()
 
     def basic_publish(self, _queue_name, _data_set):
         self.channel.queue_declare(queue=_queue_name)
@@ -38,5 +38,11 @@ class ApiCommunicator(object):
 
     def queue_consumming(self, _queue_name):
         # _queue_name = 'Measure'
-        self.channel.basic_consume(_queue_name, on_message_callback=callback, callback=measure_procedure(USER_ID))
+        self.channel.basic_consume(
+            _queue_name,
+            on_message_callback=self.callback,
+            callback=self.basic_publish(_queue_name='MeasureResult', _data_set={'UserId': self.user_id,
+                                                                                'Centimetros': self.size}
+                                        )
+        )
         self.channel.start_consuming()
